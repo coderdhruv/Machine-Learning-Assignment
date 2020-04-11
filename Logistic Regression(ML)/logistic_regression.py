41288370756ddc423329c09b1e1640016e57fe3d
coderdhruv <f20170108@hyderabad.bits-pilani.ac.in>
2020-04-11 22:12:45 +0530

    made changes to the code

--- Logistic Regression(ML)/logistic_regression.py
+++ Logistic Regression(ML)/logistic_regression.py
@@ -7,46 +7,77 @@ def sigmoid_z(z):
     return 1/(1 + np.exp(-z))
 
 
-def calc_likehihood_est(weight_list_1,pred_out_vect_arr,df):
+def calc_likehihood_est(weight_list_1,df):
     sum = 0
+    epsilon = 1e-4
+    #print(weight_list_1,"inside calc_likelihood_est")
     for i in range(0,len(df['feature1'])):
         feature_vector = list(df.iloc[i,:])
+        label = feature_vector[len(feature_vector)-1]
         feature_vector.insert(0,1)
         feature_vector = feature_vector[:len(feature_vector)-1]
         feature_vector_arr = np.asarray(feature_vector)
-        like_est = -((df['class'][i]*np.log(sigmoid_z(pred_out_vect_arr[i]))) + (1-df['class'][i])*(np.log(1 - sigmoid_z(pred_out_vect_arr[i]))))
+        #like_est = -((df['class'][i]*(np.log(sigmoid_z(np.dot(feature_vector_arr,weight_list_1))))) + (1-df['class'][i])*(np.log(1 - sigmoid_z(np.dot(feature_vector_arr,weight_list_1)))))
+        #print(np.dot(weight_list_1,feature_vector_arr),"inside calc")
+        #value = sigmoid_z(np.dot(weight_list_1,feature_vector_arr))
+        #print(value,"inside calc")
+        z = sigmoid_z(np.dot(weight_list_1,feature_vector_arr))
+        a = label*(np.log(z + epsilon))
+        b = (1 - label)*(np.log(1 - z + epsilon))
+        like_est = -a + -b
         sum = sum + like_est
     return sum    
 
 
-def logistic_regression(df,alpha):
-    weight_list = []
+def logistic_regression(df,alpha,weight_list):
+    weight_list_1 = np.asarray(list(weight_list))
+    # pred_out_vect = []
+    # for i in range(0,len(df['feature1'])):
+    #     feature_vector = list(df.iloc[i,:])
+    #     feature_vector = feature_vector[:len(feature_vector)-1]
+    #     feature_vector.insert(0,1)
+    #     feature_vector_arr = np.asarray(feature_vector)
+    #     print("featur vector",feature_vector)
+    #     pred_out_vect.append(sigmoid_z(np.dot(feature_vector_arr,weight_list_1)))
+    # pred_out_vect_arr = np.asarray(pred_out_vect)
+    orignal_cost = calc_likehihood_est(weight_list_1,df)
+    print(weight_list_1,"weight_list_1")
+    print(orignal_cost,"Before function call")
     for i in range(0,5):
-        weight_list.append(1)
-    weight_list_1 = np.asarray(weight_list)
-    pred_out_vect = []
-    for i in range(0,len(df['feature1'])):
-        feature_vector = list(df.iloc[i,:])
-        feature_vector = feature_vector[:len(feature_vector)-1]
-        feature_vector.insert(0,1)
-        feature_vector_arr = np.asarray(feature_vector)
-        pred_out_vect.append(sigmoid_z(np.dot(feature_vector_arr,weight_list_1)))
-    pred_out_vect_arr = np.asarray(pred_out_vect)
-    orignal_cost = calc_likehihood_est(weight_list_1,pred_out_vect_arr,df)
-    for i in range(0,5):
+        sum = 0
         for j in range(0,len(df['feature1'])):
-            weight_list_1[i] = weight_list_1[i] - (alpha)*(pred_out_vect_arr[j] - df['class'][j])*(df.iloc[j,:][i])
-    new_cost = calc_likehihood_est(weight_list_1,pred_out_vect_arr,df)
+            feature_vector = list(df.iloc[j,:])
+            label =  feature_vector[len(feature_vector)-1]
+            feature_vector = feature_vector[:len(feature_vector)-1]            
+            feature_vector.insert(0,1)
+            feature_vector_arr = np.asarray(feature_vector)
+            sum = sum - alpha*(sigmoid_z(np.dot(feature_vector_arr,weight_list_1)) - label)*feature_vector_arr[i]
+        weight_list_1[i] = weight_list_1[i] + sum
+    print(weight_list_1,"After modification")
+    new_cost = calc_likehihood_est(weight_list_1,df)
+    print(new_cost,"After function call")
     return orignal_cost,new_cost,weight_list_1
 
 
 file = open('./data_banknote_authentication.txt','r')
 prep = preprocess(file)
 df = prep.store_to_dataframe()
-orignal_cost,new_cost,weight_list_1 = logistic_regression(df,0.2)
+df = prep.normalize(df)
+weight_list_test = [1,0,0,0,0]
+orignal_cost,new_cost,weight_list_1 = logistic_regression(df,0.002,weight_list_test)
+print(weight_list_1)
+print(orignal_cost)
+print(new_cost)
 final_weight_list = []
-while orignal_cost - new_cost > 0.001:
-    orignal_cost,new_cost,weight_list_1 = logistic_regression(df,0.2)
-final_weight_list = weight_list_1.tolist()
-test_list = [1,-3.8203,-13.0551,16.9583,-2.3052]
-print(sigmoid_z(np.dot(final_weight_list,test_list)))
+cnt = 1
+orignal_cost = 2
+new_cost = 1
+while abs(orignal_cost - new_cost) > 0.001:
+    orignal_cost,new_cost,weight_list_1 = logistic_regression(df,0.3,weight_list_1) 
+    print("change",orignal_cost - new_cost)   
+    print(orignal_cost,new_cost,weight_list_1)
+    cnt += 1
+final_weight_list_1 = np.asarray(final_weight_list)
+test_list = [1,0.60816,7.868,2.8108,6.2612]
+test_list_arr = np.asarray(test_list)
+print(sigmoid_z(np.dot(final_weight_list_1,test_list_arr)))
--- Logistic Regression(ML)/preprocessing.py
+++ Logistic Regression(ML)/preprocessing.py
@@ -2,15 +2,30 @@ import pandas as pd
 import numpy as np
 
 class preprocess:
+        
     def __init__(self,file):
         self.file = file
     def store_to_dataframe(self):
-        read_file = pd.read_csv('./data_banknote_authentication.txt',names = ['feature1','feature2','feature3','feature4','class'])
-        print(read_file['feature1'])
-        print(read_file.iloc[0,:][0])
-        return read_file        
-        
+        self.read_file = pd.read_csv('./data_banknote_authentication.txt',names = ['feature1','feature2','feature3','feature4','class'])
+        print(self.read_file['feature1'])
+        print(self.read_file.iloc[0,:][0])
+        return self.read_file
+    def normalize(self,df):
+        max = -9999999
+        min = 9999999       
+        for i in range(0,len(df['feature1'])):
+            for j in range(0,4):
+                if df.iloc[i,:][j] > max:
+                    max = df.iloc[i,:][j]
+                if df.iloc[i,:][j] < min:
+                    min = df.iloc[i,:][j]
+        feature_vect = ['feature1','feature2','feature3','feature4']
+        for i in range(0,len(df['feature1'])):            
+            for j in range(0,4):
+                df.at[i,feature_vect[j]] = (df.iloc[i,:][j] - min)/(max - min)
+        return df
 
 file = open('./data_banknote_authentication.txt','r')
 prep = preprocess(file)
-print(prep.store_to_dataframe())
+df = prep.store_to_dataframe()
+print(prep.normalize(df).head())
