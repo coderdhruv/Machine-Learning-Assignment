5e81d3ff8c1e15dec518adbd7fe4a8e897a56a75
coderdhruv <f20170108@hyderabad.bits-pilani.ac.in>
2020-04-28 21:52:17 +0530

    added final code

--- Logistic Regression(ML)/logistic_l2_reg.py
+++ Logistic Regression(ML)/logistic_l2_reg.py
@@ -8,8 +8,11 @@ def sigmoid_z(z):
     return 1/(1 + np.exp(-z))
     
 
-def calc_likehihood_est(weight_list_1,df):
+def calc_likehihood_est(weight_list_1,df,lmbda):    
     sum = 0
+    reg_term = 0
+    for i in range(0,len(weight_list_1)):
+        reg_term = reg_term + (lmbda*(weight_list_1[i])*(weight_list_1[i]))
     epsilon = 1e-5
     n = len(df['feature1'])
     for i in range(0,len(df['feature1'])):
@@ -23,12 +26,12 @@ def calc_likehihood_est(weight_list_1,df):
         b = (1 - label)*(np.log(1 - z + epsilon))
         like_est = (-a + -b)/(2*n)
         sum = sum + like_est
-    return sum    
+    return sum + reg_term    
 
 
-def logistic_regression(df,alpha,weight_list):
+def logistic_regression(df,alpha,weight_list,lmbda):
     weight_list_1 = np.array(list(weight_list))
-    orignal_cost = calc_likehihood_est(weight_list_1,df)
+    orignal_cost = calc_likehihood_est(weight_list_1,df,1)
     print(weight_list_1,"weight_list_1")
     print(orignal_cost,"Before function call")
     for i in range(0,5):
@@ -39,10 +42,10 @@ def logistic_regression(df,alpha,weight_list):
             feature_vector = feature_vector[:len(feature_vector)-1]            
             feature_vector.insert(0,1)
             feature_vector_arr = np.asarray(feature_vector)
-            sum = sum - alpha*(sigmoid_z(np.dot(feature_vector_arr,weight_list_1)) - label)*feature_vector_arr[i]
-        weight_list_1[i] = weight_list_1[i] + sum
+            sum = sum - alpha*(((sigmoid_z(np.dot(feature_vector_arr,weight_list_1)) - label)*feature_vector_arr[i]))
+        weight_list_1[i] = weight_list_1[i] + sum - alpha*(lmbda*weight_list_1[i])
     print(weight_list_1,"After modification")
-    new_cost = calc_likehihood_est(weight_list_1,df)
+    new_cost = calc_likehihood_est(weight_list_1,df,1)
     print(new_cost,"After function call")
     return orignal_cost,new_cost,weight_list_1
 
@@ -52,7 +55,7 @@ prep = preprocess(file)
 df = prep.store_to_dataframe()
 df1,df2 = prep.test_train_data_set(df,0.2)
 weight_list_test = [1,0,0,0,0]
-orignal_cost,new_cost,weight_list_1 = logistic_regression(df1,0.4,weight_list_test)
+orignal_cost,new_cost,weight_list_1 = logistic_regression(df1,0.01,weight_list_test,1)
 print(weight_list_1)
 print(orignal_cost)
 print(new_cost)
@@ -61,8 +64,8 @@ cnt = 1
 orignal_cost = 2
 new_cost = 1
 epoch = 0
-while (orignal_cost - new_cost) > 0.005 and epoch < 40:
-    orignal_cost,new_cost,weight_list_1 = logistic_regression(df1,0.4,weight_list_1)
+while (orignal_cost - new_cost) > 0.005 and epoch < 100:
+    orignal_cost,new_cost,weight_list_1 = logistic_regression(df1,0.01,weight_list_1,1)
     epoch += 1 
     print("change",orignal_cost - new_cost)   
     print(orignal_cost,new_cost,weight_list_1)
